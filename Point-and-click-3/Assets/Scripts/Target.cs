ea5ebb0157f68eafe882b81837de53b41ae1d1b6
HenriqueHaagS <138019535+HenriqueHaagS@users.noreply.github.com>
2023-07-19 11:32:52 -0300

    Player movement, colliders

---
+++ Point-and-click-3/Assets/Scripts/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Vector2 followSpot;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followSpot = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            followSpot = new Vector2(mousePosition.x, mousePosition.y);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, followSpot, Time.deltaTime * speed);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        followSpot = transform.position;
+    }
+}
